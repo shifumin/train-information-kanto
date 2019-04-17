a17edd78d3f363280fc8c35e448ae638c05bdb79
shifumin <shifumin4230@gmail.com>
2019-04-17 20:56:59 +0900

    update getting routes

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
 end
 
-texts = TraininfoKanto.get(%w[山手線 京浜東北線 横須賀線 中央線 中央総武線 総武快速線 内房線 京葉線 銀座線 千代田線], url: true)
+texts = TraininfoKanto.get(%w[山手線 京浜東北線 横須賀線 中央線 中央総武線 総武快速線 内房線 京葉線 丸ノ内線 有楽町線], url: true)
 client = Slack::Web::Client.new
 
 texts.each do |text|
-  client.chat_postMessage(channel: '#test', text: text, as_user: true)
+  client.chat_postMessage(channel: '#bot', text: text, as_user: true)
 end
