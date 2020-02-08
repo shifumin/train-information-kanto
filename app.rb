c01bece8268e4cfb5233ee5123017d524c5b263f
shifumin <shifumin4230@gmail.com>
2020-02-08 18:50:48 +0900

    update post message format

--- app.rb
+++ app.rb
@@ -10,6 +10,5 @@ end
 texts = TraininfoKanto.get(%w[山手線 京浜東北線 横須賀線 中央線 中央総武線 総武快速線 内房線 京葉線 丸ノ内線 有楽町線], url: true)
 client = Slack::Web::Client.new
 
-texts.each do |text|
-  client.chat_postMessage(channel: '#bot', text: text, as_user: true)
-end
+# FIXME: gemのレスポンスを適切に修正した上で1つのmessageに収まるようにpostする
+client.chat_postMessage(channel: '#bot', text: texts.join("\n"), as_user: true)
